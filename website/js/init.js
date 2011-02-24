a61b62e21c5cdc76da2a803d371e004e7feb383d
tav <tav@espians.com>
2011-02-24 09:02:10 +0000

    Worked around ClearType issues reported by endangeredmassa.

--- website/js/init.js
+++ website/js/init.js
@@ -129,3 +129,95 @@ setupGoogleAnalytics();
 
 $(startup);
 
+// -----------------------------------------------------------------------------
+// Font Smoothing Detection
+// -----------------------------------------------------------------------------
+
+// Adapted from
+// http://www.useragentman.com/blog/2009/11/29/how-to-detect-font-smoothing-using-javascript/
+
+function detectFontSmoothing() {
+
+  // IE can be surprisingly helpful sometimes.
+  if (screen && typeof(screen.fontSmoothingEnabled) !== "undefined")
+    return screen.fontSmoothingEnabled;
+
+  var canvas = document.createElement('canvas'),
+      ctx,
+      script;
+
+  // Assume that non-IE older browsers are not running on Windows.
+  if (!canvas.getContext)
+    return null;
+
+  try {
+
+    // Create a 35x35 Canvas block.
+    canvas.width = "35";
+    canvas.height = "35";
+
+    // We must put this node into the body, otherwise
+    // Safari Windows does not report correctly.
+    canvas.style.display = 'none';
+    script = document.getElementsByTagName('script')[0];
+    script.parentNode.insertBefore(canvas, script);
+
+    // Draw a black letter 'O', 32px Arial.
+    ctx = canvas.getContext('2d');
+    ctx.textBaseline = "top";
+    ctx.font = "12px Arial";
+    ctx.fillStyle = "black";
+    ctx.strokeStyle = "black";
+    ctx.fillText("O", 0, 0);
+
+    // Start at (8,1) and search the canvas from left to right,
+    // top to bottom to see if we can find a non-black pixel. If
+    // so we return true.
+    for (var j=8; j <= 32; j++) {
+      for (var i = 1; i <= 32; i++) {
+        var imageData = ctx.getImageData(i, j, 1, 1).data,
+            alpha = imageData[3];
+        if (alpha != 255 && alpha != 0)
+            return true;
+      }
+    }
+  } catch (ex) {
+    // Something went wrong (for example, Opera cannot use the
+    // canvas fillText() method.  Return null (unknown).
+    return null;
+  }
+
+  return false;
+
+};
+
+// -----------------------------------------------------------------------------
+// Handle Fonts
+// -----------------------------------------------------------------------------
+
+function loadStylesheet(_rules) {
+  var css = document.createElement('style'),
+      rules = document.createTextNode(_rules),
+      script = document.getElementsByTagName('script')[0];
+  css.type = "text/css";
+  if (css.styleSheet) {
+    css.styleSheet.cssText = rules.nodeValue;
+  } else {
+    css.appendChild(rules);
+  }
+  script.parentNode.insertBefore(css, script);
+};
+
+if (!(detectFontSmoothing() === false)) {
+  try {
+    Typekit.load();
+    if ($.browser.msie) {
+      loadStylesheet('code, pre, .literal { font-size: 17px; line-height: 20px; }');
+    }
+  } catch(e) {}
+} else {
+  try {
+    loadStylesheet('code, pre, .literal { font-family: Monaco, "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Courier New", monospace; font-size: 14px; line-height: 16px; } body, #intro h1 { font-family: Verdana, Arial, sans-serif; font-size: 13px; line-height: 23px; } .content { line-height: 23px; } em { font-style: normal !important; font-weight: bold; } .article-info { font-size: 11px; } #site-title { font-size: 54px; line-height: 60px; padding-bottom: 8px; } intro { font-size: 15px; } #intro strong { font-size: 24px; } .content a, #intro a, .article-nav a, .additional-content-info a, #disqus-comments-section a { font-weight: bold; }}');
+    Typekit.load();
+  } catch(e) {}
+}
